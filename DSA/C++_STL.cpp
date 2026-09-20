626c7a9c3490c2903579f787fd11f28bf111f724
Prashant-creater233 <prashantkanwal8@gmail.com>
2026-09-20 19:32:55 +0530

    Remove unused executable files and clean up commented code in C++ STL implementation

--- DSA/C++_STL.cpp
+++ DSA/C++_STL.cpp
@@ -3,6 +3,10 @@
 #include <stack>
 #include <queue>
 #include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
 
 // vector container
@@ -179,32 +183,155 @@ using namespace std;
 
 // Map
 
+// int main(){
+//     map<string, int> m;
+
+//     m["tv"] = 100;
+//     m["laptop"] = 100;
+//     m["laptop"] = 300;
+//     m["headphones"] = 50;
+//     m["table"] = 120;
+//     m["watch"] = 50;
+
+//     m.insert({"camera", 25});
+//     m.emplace("glass", 40);
+//     m.erase("tv");
+
+//     for(auto p: m) {
+//         cout << p.first << " " << p.second << endl;
+//     }
+
+//     cout << "count = " << m.count("laptop") << endl;
+//     cout << "laptop value = " << m["laptop"] << endl;
+
+//     if(m.find("camera") != m.end()){
+//         cout << "found\n";
+//     }else{
+//         cout << "Not found";
+//     }
+
+//    return 0;
+// }
+
+
+// multimap
+// int main(){
+//     multimap<string, int> m;
+
+//     m.insert({"camera", 25});
+//     m.emplace("glass", 40);
+//     m.emplace("glass", 40);
+//     m.emplace("glass", 40);
+//     m.emplace("glass", 50);
+
+//     //m.erase("glass"); // sari delete ho jayenge
+//     m.erase(m.find("glass")); // ek tv delete hoga
+
+//     for(auto p: m) {
+//         cout << p.first << " " << p.second << endl;   
+//     }
+//    return 0;
+// }
+
+
+// // unordered map store in unordered way
+// int main(){
+//     unordered_map<string, int> m;
+
+//     m.insert({"camera", 25});
+//     m.emplace("glass", 40);
+//     m.emplace("laptop", 140);
+//     m.emplace("fridge", 100);
+//     m.emplace("watch", 150);
+
+//     for(auto p: m) {
+//         cout << p.first << " " << p.second << endl;   
+//     }
+//    return 0;
+// }
+
+
+
+// set
+// int main(){
+//     set<int> s;
+
+//     s.insert(1);
+//     s.insert(2);
+//     s.insert(3);
+//     s.insert(4);
+//     s.insert(5);
+
+//     // s.insert(1);
+//     // s.insert(2);
+//     // s.insert(3);
+
+
+//     cout << "lower bound = " << *(s.lower_bound(4)) << endl; //4 if 4 not in set 4 se just badi value 5 aata agar koi bhi badi value exit nhi krti to return s.end() return anyvalue we don't know
+//     cout << "upper bound = " << *(s.upper_bound(4)) << endl; // 5 
+//     for(auto val: s) {
+//         cout << val << " ";
+//     }
+//     cout << endl;
+//    return 0;
+// }
+
+
+// unordered set
+// int main(){
+//     unordered_set<int> s;
+
+//     s.insert(1);
+//     s.insert(2);
+//     s.insert(3);
+//     s.insert(4);
+//     s.insert(5); 
+
+//     s.insert(1);
+//     s.insert(2);
+//     s.insert(3);
+
+//     for(auto val: s) {
+//         cout << val << " ";
+//     }
+//     cout << endl;
+//    return 0;
+// }
+
+
+
+// Algorithms
+// 1 sort 
+
+// int main(){
+//     // int arr[5] =  {3, 5, 1, 8, 2};
+
+//     // sort(arr, arr + 5);
+
+//     // for(int val : arr) {
+//     //     cout << val << " ";
+//     // }
+//     // cout << endl;
+
+//     vector<int> vec =  {3, 5, 1, 8, 2};
+
+//     // sort(vec.begin(), vec.end()); // ascending order
+//     sort(vec.begin(), vec.end(), greater<int>()); // decendind order
+
+//     for(int val : vec){
+//         cout << val << " ";
+//     }
+//     cout << endl;
+// }
+
 int main(){
-    map<string, int> m;
 
-    m["tv"] = 100;
-    m["laptop"] = 100;
-    m["laptop"] = 300;
-    m["headphones"] = 50;
-    m["table"] = 120;
-    m["watch"] = 50;
+    vector<pair<int, int>>  vec =  {{3, 1}, {2, 1}, {7, 1}, {5, 2}};
 
-    m.insert({"camera", 25});
-    m.emplace("glass", 40);
-    m.erase("tv");
+    sort(vec.begin(), vec.end());
 
-    for(auto p: m) {
-        cout << p.first << " " << p.second << endl;
+    for(auto p : vec){
+        cout << p.first << " " << p.second << endl;  // sort in ascending on the basis of first value but if i want to arrange acc to second value so we want to make custom comparator
     }
-
-    cout << "count = " << m.count("laptop") << endl;
-    cout << "laptop value = " << m["laptop"] << endl;
-
-    if(m.find("camera") != m.end()){
-        cout << "found\n";
-    }else{
-        cout << "Not found";
-    }
-
-   return 0;
+    cout << endl;
 }
--- DSA/tempCodeRunnerFile.cpp
+++ DSA/tempCodeRunnerFile.cpp
@@ -1,20 +1,2 @@
-int main(){
-    int arr[]= {1,2,5,3,1,2,3,4,6};
-    int size = 9;
 
-    for(int i=0; i<size; i++){
-        int count=0;
-
-        for(int j=0; j<size; j++){
-            if(arr[i] == arr[j]){
-                count++;
-            }
-        }
-
-        if(count == 1){
-            cout << arr[i] << " ";
-        }
-    }
-
-    return 0;
-}
+    // s.insert(5);
